a245aedca280f761597b311b2850fdff19e903af
d3v-tushar <d3v-tushar@gmail.com>
2022-08-23 21:44:29 +0600

    Code Beautified

--- script/index.js
+++ script/index.js
@@ -1,6 +1,5 @@
 document.getElementById('btn-messi').addEventListener('click', function(){
     topFivePlayers('leo-messi', 'btn-messi');
-    
 })
 
 document.getElementById('btn-neymar').addEventListener('click', function(){
@@ -12,26 +11,6 @@ document.getElementById('btn-mbappe').addEventListener('click', function(){
 })
 
 // OnClick Player Selection Function
-// function elementLength(){
-//     const li = document.querySelectorAll('li');
-//     for(lis of li){
-//     }
-//     return li.length;
-// }
-// const playerCount = elementLength();
-// console.log(elementLength());
-
-// function topFivePlayers(playerName, playerButtonId){
-//     const playersName = document.getElementById(playerName);
-//     const playerValue = playersName.innerText;
-//     const addPlayers = document.createElement('li');
-//     addPlayers.innerText = playerValue;
-//     const playersContainer = document.getElementById('player-box');
-//     playersContainer.appendChild(addPlayers);
-//     const disableButton = document.getElementById(playerButtonId);
-//     disableButton.setAttribute("disabled", "");
-// }
-
 function topFivePlayers(playerName, playerButtonId){
     const playersContainer = document.getElementById('player-box');
     if(playersContainer.children.length > 4){
